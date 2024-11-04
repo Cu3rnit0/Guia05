69d81d4a6c3491a41e92cac7905341b05dd700f3
Calzadilla <cg19i04001@usonsonate.edu.sv>
2024-11-04 13:53:42 -0600

    Update_MainActivity

--- app/src/main/java/com/quickprogram/sqlite/MainActivity.java
+++ app/src/main/java/com/quickprogram/sqlite/MainActivity.java
@@ -1,6 +1,10 @@
 package com.quickprogram.sqlite;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +14,45 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button btnAgregarPersona;
+    private Button btnMostrarLista;
+    private Button btnAcerca;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        btnAgregarPersona = findViewById(R.id.btnAgregarPersona);
+        btnMostrarLista = findViewById(R.id.btnMostrarLista);
+        btnAcerca = findViewById(R.id.btnAcerca);
+        btnAgregarPersona.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    Intent intent = new Intent(MainActivity.this, AgregarPersonaActivity.class);
+                    startActivity(intent);
+                } catch (Exception e) {
+                    Log.e("AgregarPersonaActivity", "Error al guardar persona: " + e.getMessage());
+                    e.printStackTrace();
+                }
+            }
+
+        });
+
+        btnMostrarLista.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MostrarListaActivity.class);
+                startActivity(intent);
+            }
+        });
+        btnAcerca.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, AcercaDeActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
