b1880c3b7532e603211777756c1bb6cc70334ff5
Calzadilla <cg19i04001@usonsonate.edu.sv>
2024-11-04 13:19:26 -0600

    Update_PersonaENTITY

--- app/src/main/java/com/quickprogram/sqlite/Entidades/Persona.java
+++ app/src/main/java/com/quickprogram/sqlite/Entidades/Persona.java
@@ -1,4 +1,37 @@
 package com.quickprogram.sqlite.Entidades;
 
-public class Persona {
+import androidx.annotation.NonNull;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+import java.io.*;
+
+@Entity
+public class Persona implements Serializable {
+    @PrimaryKey(autoGenerate = true)
+    public int idPersona;
+    public String nombrePersona;
+    public String apellidosPersona;
+    public int edadPersona;
+
+    public Persona(){
+
+    }
+    public  Persona(String nombrePersona,String apellidosPersona, int edadPersona){
+        this.nombrePersona =nombrePersona;
+        this.apellidosPersona = apellidosPersona;
+        this.edadPersona = edadPersona;
+    }
+
+    public Persona(int idPersona, String nombrePersona, String apellidosPersona, int edadPersona){
+        this.idPersona= idPersona;
+        this.nombrePersona= nombrePersona;
+        this.apellidosPersona = apellidosPersona;
+        this.edadPersona= edadPersona;
+    }
+
+@NonNull
+@Override
+public String toString(){
+return nombrePersona+""+apellidosPersona+""+edadPersona;}
+
 }
